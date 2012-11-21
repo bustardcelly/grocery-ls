0302a8fecb56ad9f3b71acd57f601271084eb6b3
Todd Anderson <bustardcelly@gmail.com>
2012-11-21 11:24:45 -0500

    update for specs on add item and creation.

--- test/jasmine/spec/newitem.spec.js
+++ test/jasmine/spec/newitem.spec.js
@@ -1,56 +1,77 @@
 define( function() {
 
-  describe('User requests to add new item', function() {
-
-    var service = {
-          addItem: function(value) {
-          }
-        },
-        listController = {
-          service: service,
-          editableItem: undefined,
-          addEditableItemToList: function() {
-            this.editableItem = Object.create(null);
-          },
-          saveEditableItem: function() {
-            return service.addItem(this.editableItem);
-          }
-        },
-        addItemStub,
-        async = new AsyncSpec(this);
-
-    beforeEach( function() {
-      addItemStub = sinon.stub( service, "addItem", function() {
+  var itemProperties = function(id) {
         return {
-          on: function( responseType, handler ) {
-            var timeout = setTimeout( function() {
-              clearTimeout(timeout);
-              handler.call(null, true);
-            }, 1000);
+          "id": {
+            value: id,
+            writable: false,
+            enumerable: true
+          },
+          "name": {
+            value: '',
+            writable: true,
+            enumerable: true
           }
         };
-      });
+      },
+      listController = {
+        itemList: [],
+        editableItem: undefined,
+        createNewItem: function() {
+          this.editableItem = Object.create(Object.prototype, itemProperties(new Date().getTime()));
+        },
+        editFocusedItem: function(name) {
+          this.editableItem.name = name;
+        },
+        saveFocusedItem: function() {
+          if( this.editableItem.name.length !== 0 ) {
+            this.itemList.push(this.editableItem);
+          }
+          this.editableItem = undefined;
+        }
+      },
+      itemName = 'apples';
+
+  describe('List Controller creates a new item', function() {
+
+    beforeEach( function() {
+      listController.createNewItem();
     });
 
-    it('should create a new editable item', function() {
-      var editableItem = listController.addEditableItemToList();
+    it('should expose the editableItem upon creation', function() {
       expect(listController.editableItem).not.toBeUndefined();
-      expect(service.addItem.called).toBe(false);
-    });
-
-    async.it('should save new item last created', function(done) {
-      var request;
-      listController.addEditableItemToList();
-      request = listController.saveEditableItem();
-      request.on( 'complete', function(result) {
-        expect(service.addItem.calledOnce).toBe(true);
-        expect(service.addItem.calledWith(listController.editableItem)).toBe(true);
-        done();
-      });
     });
 
     afterEach( function() {
-      addItemStub.restore();
+      listController.editableItem = undefined;
+    });
+
+  });
+
+  describe('User requests to add new item', function() {
+
+    beforeEach( function() {
+      listController.createNewItem();
+      listController.editFocusedItem(itemName);
+      listController.saveFocusedItem();
+    });
+
+    it('should save new item when name supplied', function() {
+      expect(listController.itemList.length).toBe(1);
+      expect(listController.itemList[0]).not.toBe(undefined);
+      expect(listController.itemList[0].hasOwnProperty('name')).toBe(true);
+      expect(listController.itemList[0].name).toBe(itemName);
+    });
+
+    it('should not save new item when name not supplied', function() {
+      listController.createNewItem();
+      listController.saveFocusedItem();
+      expect(listController.itemList.length).toBe(1);
+      expect(listController.itemList[0].name).toBe(itemName);
+    });
+
+    afterEach( function() {
+      listController.itemList = [];
       listController.editableItem = undefined;
     });
 
