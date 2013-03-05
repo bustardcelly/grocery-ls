24870590605a95a0f5aa4180838d4f55ebd2d842
Todd Anderson <bustardcelly@gmail.com>
2013-03-05 06:24:43 -0500

    modified storage-service to communicate with localStorage.

--- script/service/storage-service.js
+++ script/service/storage-service.js
@@ -1,35 +1,86 @@
-define(['jquery'], function($) {
+define(['jquery', 'script/model/grocery-ls-item'], function($, modelFactory) {
   
-  var itemCache = [],
+  var itemCache,
+      groceryListKey = 'com.custardbelly.grocerylist',
+      parseToCollection = function(json) {
+        var i,
+            length,
+            list = (json && typeof json === 'string') ? JSON.parse(json) : [];
+
+        for(i = 0; i < length; i++) {
+          list[i] = $.extend(modelFactory.create(), list[i]);
+        }
+        return list;
+      },
+      serialize = function(key, data) {
+        window.localStorage.setItem(key, JSON.stringify(data));
+      },
       store = {
         saveItem: function(item) {
-          var deferred = $.Deferred(),
-              index = itemCache.indexOf(item);
-          if(index === -1) {
-            itemCache[itemCache.length] = item;
-          }
-          return deferred.resolve(item);
+          var deferred = $.Deferred();
+          $.when(this.getItems()).then(function(cache) {
+            var index = cache.indexOf(item);
+            try {
+              if(index === -1) {
+                cache[cache.length] = item;
+              }
+              serialize(groceryListKey, cache);
+              deferred.resolve(item);
+            }
+            catch(e) {
+              deferred.reject('Could not save item: ' + e.message);
+            }
+          });
+          return deferred;
         },
         removeItem: function(item) {
-          var deferred = $.Deferred(),
-              itemIndex = itemCache.indexOf(item),
-              removedItem;
-
-          if(itemIndex > -1) {
-            itemCache.splice(itemIndex, 1);
-            removedItem = item;
-          }
-          return deferred.resolve(removedItem);
+          var deferred = $.Deferred();
+          $.when(this.getItems()).then(function(cache) {
+            var itemIndex = cache.indexOf(item),
+                removedItem;
+            try {
+              if(itemIndex > -1) {
+                cache.splice(itemIndex, 1);
+                removedItem = item;
+                serialize(groceryListKey, cache);
+              }
+              deferred.resolve(removedItem);
+            }
+            catch(e) {
+              cache.splice(itemIndex, 0, removedItem);
+              deferred.reject('Could not remove item: ' + e.message);
+            }
+          });
+          return deferred;
         },
         getItems: function() {
           var deferred = $.Deferred();
-          deferred.resolve(itemCache);
+          if(itemCache === undefined) {
+             try {
+              itemCache = parseToCollection(window.localStorage.getItem(groceryListKey));
+              deferred.resolve(itemCache);
+            }
+            catch(e) {
+              deferred.reject('Could not access items: ' + e.message);
+            }
+          }
+          else {
+            deferred.resolve(itemCache);
+          }
           return deferred;
         },
         empty: function() {
           var deferred = $.Deferred();
-          itemCache.length = 0;
-          deferred.resolve(itemCache);
+          $.when(this.getItems()).then(function(cache) {
+            try {
+              cache.length = 0;
+              serialize(groceryListKey, cache);
+              deferred.resolve(cache);
+            }
+            catch(e) {
+              deferred.reject('Could not empty cache: ' + e.message);
+            }
+          });
           return deferred;
         }
       };
