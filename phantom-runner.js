9fe139a4bf2d81fc313b4d896b0296b55f61cc95
Todd Anderson <bustardcelly@gmail.com>
2013-10-03 13:24:31 -0400

    Update phantom-runner.js
    pointing to jasmine test runner in github.

--- phantom-runner.js
+++ phantom-runner.js
@@ -1,14 +1,14 @@
 var page = new WebPage();
-var url = "file://localhost/Users/danmerino/test/teamcity_reporter.html";
+var url = "https://raw.github.com/bustardcelly/grocery-ls/master/test/jasmine/specrunner.html";
 phantom.viewportSize = {width: 800, height: 600};
 page.onConsoleMessage = function (msg) { console.log(msg); };
 page.open(url, function (status) {
         if (status !== 'success') {
                 console.log('Unable to load the address!');
-            } else {
+        } else {
                window.setTimeout(function () {
                     page.render("output.png");
                     phantom.exit();
                }, 200);
-            }
+        }
 });
